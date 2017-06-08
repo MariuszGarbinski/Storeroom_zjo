cad7b22d96dd7fbd17c56628a5c34831df8d875d
emlody <emlody@gmail.com>
2017-06-09 01:41:56 +0200

    Logowanie

--- Storeroom_zjo/App_Start/RouteConfig.cs
+++ Storeroom_zjo/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace Storeroom_zjo
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              // defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
